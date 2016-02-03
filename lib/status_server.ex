db4ee7bccd3cd6428b419a70ffc820274efa9d32
Sheldon Kreger <sheldonkreger@gmail.com>
2016-02-02 22:23:27 -0800

    Use a case statement to set state equal to status.code

--- lib/status_server.ex
+++ lib/status_server.ex
@@ -17,8 +17,25 @@ defmodule StatusServer do
   end
 
   def handle_info(url, state) do
-    out = HTTPoison.head(url, [], [follow_redirect: true, max_redirect: 1])
-    IO.inspect(out)
-    {:noreply, state}
+    # Fooling around with accessing values in a stuct. Good times.
+    # response = HTTPoison.head(url, [], [follow_redirect: true, max_redirect: 1])
+    # var = Keyword.get(response, :headers)
+    # IO.inspect(elem(response,1))
+    # var = elem(response, 1)
+    # IO.inspect(elem(response, 1).status_code)
+    # Keyword.get(var.headers, :status)
+    case HTTPoison.head(url) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        IO.inspect(200)
+        new_state = 200
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+        new_state = 404
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+        new_state = -1
+    end
+    # new_state = state
+    {:noreply, new_state}
   end
 end
